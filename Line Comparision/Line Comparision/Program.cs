85a32e0c0365681d8c6c33ff937f80a5bf6b555f
madhums0411 <madhums41175@gmail.com>
2022-07-18 17:38:10 +0530

    Line Length Calculation

--- Line Comparision/Line Comparision/Program.cs
+++ Line Comparision/Line Comparision/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Welcome to Line Comparision");
+﻿class LineComparision
+{
+    public static void Main(string[] args)
+    {
+
+        Console.WriteLine("Enter Value X1");
+        int X1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter Value Y1");
+        int Y1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter Value X2");
+        int X2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter Value Y2");
+        int Y2 = Convert.ToInt32(Console.ReadLine());
+        double Length = Math.Sqrt((X2 - X1) ^ 2 + (Y2 - Y1) ^ 2);
+        Console.WriteLine("Length is: " + Length);
+    }
+
+ }
