42aeb1577e019c73d7a2d316e1d4b68fa033a56e
madhums0411 <madhums41175@gmail.com>
2022-07-18 20:17:26 +0530

    Check the equality of two lines

--- Line Comparision/Line Comparision/Program.cs
+++ Line Comparision/Line Comparision/Program.cs
@@ -3,6 +3,8 @@
     public static void Main(string[] args)
     {
 
+        Console.WriteLine("Enter the Co-ordinates of first line");
+
         Console.WriteLine("Enter Value X1");
         int X1 = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("Enter Value Y1");
@@ -12,8 +14,32 @@
         Console.WriteLine("Enter Value Y2");
         int Y2 = Convert.ToInt32(Console.ReadLine());
 
-        double Length = Math.Sqrt((X2 - X1) ^ 2 + (Y2 - Y1) ^ 2);
-        Console.WriteLine("Length of line is: " + Length);
+        double Length1 = Math.Sqrt((X2 - X1) ^ 2 + (Y2 - Y1) ^ 2);
+        Console.WriteLine("Length of first line is: " + Length1);
+
+        Console.WriteLine("Enter the Co-ordinates of second line");
+
+        Console.WriteLine("Enter Value X1");
+        int A1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter Value Y1");
+        int B1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter Value X2");
+        int A2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Enter Value Y2");
+        int B2 = Convert.ToInt32(Console.ReadLine());
+
+        double Length2 = Math.Sqrt((A2 - A1) ^ 2 + (B2 - B1) ^ 2);
+        Console.WriteLine("Length of second line is: " + Length2);
+
+        if (Length1 == Length2)
+        {
+            Console.WriteLine("The two lines are equal in length");
+        }
+        else
+        {
+            Console.WriteLine("The two lines are not equal in lentgh");
+        }
+
 
     }
 
